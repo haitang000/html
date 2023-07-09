67d62f4cd842306b62d57aec1884d1ebe7d0c18e
wlingzhenyu <wlingzhenyu@users.noreply.github.com>
2023-07-09 15:24:49 +0000

    deploy: 81ade96a496c42dd43796a31cc2aa587d58cbb57

---
+++ templates/config.py
@@ -0,0 +1,9 @@
+import pymysql
+
+conn = pymysql.connect(
+        host='49.232.240.247',
+        port=3306,
+        user='zlhtml',
+        password='cMdXkMfA5s7ipzxj',
+        database='zlhtml'
+)
